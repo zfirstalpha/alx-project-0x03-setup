12742756131acdc4c9e0cb18d598de8a598385a9
zfirstalpha <salim14sadik@gmail.com>
2025-11-11 15:10:13 +0300

    setup landing page with imperative routing

--- alx-project-0x03/components/layouts/Layout.tsx
+++ alx-project-0x03/components/layouts/Layout.tsx
@@ -1,10 +1,6 @@
-import { ReactNode } from "react";
+import { LayoutProps } from "@/interface";
 import Footer from "./Footer";
 import Header from "./Header";
-
-interface LayoutProps {
-  children: ReactNode;
-}
 
 const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
